13848a76ab2d2928927aaefca0c9bcd0d26c20c2
UchuNoKen <vanguardbosses@gmail.com>
2018-09-03 13:34:08 -0400

    Modified index.js to require cluster and forked master

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-const express = require('express');
-const app = express();
+const cluster = require('cluster');
 
-function doWork(duration){
+// Is file being executed in master mode?
+if(cluster.isMaster){
+  // Cause index.js to be executed again but in child mode
+  cluster.fork();
+}else{
+  // Child mode, will act like a server and do nothing else
+  const express = require('express');
+  const app = express();
+    
+  function doWork(duration){
     const start = Date.now();
-
+    
     while(Date.now() - start < duration){
-
+    
     }
+  }
+    
+    app.get('/', (req, res) => {
+        doWork(5000);
+        res.send('Hi there');
+    });
+    
+    app.listen(3000);
 }
 
-app.get('/', (req, res) => {
-    doWork(5000);
-    res.send('Hi there');
-});
-
-app.listen(3000);
