c1a3bcc6b4464f2a8ef788d3e4109a7316668d07
UchuNoKen <vanguardbosses@gmail.com>
2018-09-03 17:49:41 -0400

    Decreased clusters to two

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cluster = require('cluster');
 if(cluster.isMaster){
   // Cause index.js to be executed again but in child mode
   cluster.fork();
+  cluster.fork();
 }else{
   // Child mode, will act like a server and do nothing else
   const express = require('express');
