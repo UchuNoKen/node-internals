6a59b5127948f78bba04f9b84e307b4f9aaa36ba
UchuNoKen <vanguardbosses@gmail.com>
2018-09-03 14:10:16 -0400

    Removed doWork function, required crypto module, added call to pbkdf2 within the root route

--- index.js
+++ index.js
@@ -1,28 +1,21 @@
+process.env.UV_THREADPOOL_SIZE = 1;
 const cluster = require('cluster');
+
 
 // Is file being executed in master mode?
 if(cluster.isMaster){
   // Cause index.js to be executed again but in child mode
   cluster.fork();
-//   cluster.fork();
-//   cluster.fork();
-//   cluster.fork();
 }else{
   // Child mode, will act like a server and do nothing else
   const express = require('express');
+  const crypto = require('crypto');
   const app = express();
     
-  function doWork(duration){
-    const start = Date.now();
-    
-    while(Date.now() - start < duration){
-    
-    }
-  }
-    
     app.get('/', (req, res) => {
-        doWork(5000);
-        res.send('Hi there');
+        crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+            res.send('Hi there');
+        }); 
     });
 
     app.get('/fast', (req, res) => {
